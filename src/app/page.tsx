a26a264dc2dbedab303195611faaafc4334ce57f
Ângelo Marques <angeloemanuelmarques@gmail.com>
2025-03-12 20:11:13 -0300

    chore: set up trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 // app/page.tsx
 
+import { api, HydrateClient } from "@/trpc/server";
 import Chat from "./chat";
 
-export default function Page() {
-  return <Chat />;
+export default async function Page() {
+  const hello = await api.post.hello({ text: "from Ângelo" });
+
+  return (
+    <HydrateClient>
+      {hello.greeting}
+      <Chat />
+    </HydrateClient>
+  );
 }
